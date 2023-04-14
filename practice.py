ef59fb1150d0d8ab8523c7372f71a916eabfcbee
Ocansey Abdul Wahab <ocanseyabdulwahab2@gmail.com>
2023-04-14 08:39:19 +0000

    added surname

--- practice.py
+++ practice.py
@@ -2,6 +2,7 @@ name=input('My name is: ', {'name'})
 Full_Name=input('             : is my name')
 Age=input('I am : ',{'years old'})
 level_of_education=input('Enter your level of education: ')
+Surname=input('Enter your surname: ')
 
 
 
