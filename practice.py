83eedc3457b20fddece33452bedfe9be9c075c20
Ocansey Abdul Wahab <ocanseyabdulwahab2@gmail.com>
2023-04-14 08:15:14 +0000

    practice.py

--- practice.py
+++ practice.py
@@ -1,5 +1,7 @@
 name=input('My name is: ', {'name'})
 Full_Name=input('             : is my name')
 Age=input('I am : ',{'years old'})
+level_of_education=input('Enter your level of education: ')
+
 
 
